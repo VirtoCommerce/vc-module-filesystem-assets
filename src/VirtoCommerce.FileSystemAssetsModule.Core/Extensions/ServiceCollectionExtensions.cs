3f87febba00ea977ba9596217822e048f040b6c8
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-11-12 10:45:09 +0200

    feat: Implement Common provider abstraction

--- src/VirtoCommerce.FileSystemAssetsModule.Core/Extensions/ServiceCollectionExtensions.cs
+++ src/VirtoCommerce.FileSystemAssetsModule.Core/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
-using System;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using VirtoCommerce.Assets.Abstractions;
 using VirtoCommerce.AssetsModule.Core.Assets;
 
 namespace VirtoCommerce.FileSystemAssetsModule.Core.Extensions
@@ -8,6 +9,7 @@ namespace VirtoCommerce.FileSystemAssetsModule.Core.Extensions
     {
         public static void AddFileSystemBlobProvider(this IServiceCollection services, Action<FileSystemBlobOptions> setupAction = null)
         {
+            services.AddSingleton<ICommonBlobProvider, FileSystemBlobProvider>();
             services.AddSingleton<IBlobStorageProvider, FileSystemBlobProvider>();
             services.AddSingleton<IBlobUrlResolver, FileSystemBlobProvider>();
             if (setupAction != null)
